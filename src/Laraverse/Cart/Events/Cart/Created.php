79b18ce646fecfd781c9abe2855ff85933379d4d
Jonathan Garbee <jonathan@garbee.me>
2015-02-01 06:16:41 -0500

    Filled in some event constructors.

--- src/Laraverse/Cart/Events/Cart/Created.php
+++ src/Laraverse/Cart/Events/Cart/Created.php
@@ -1,6 +1,11 @@
 <?php namespace Laraverse\Cart\Events\Cart;
 
+use Laraverse\Cart\Collections\Cart;
 
 class Created {
+    public $cart;
 
+    public function __construct(Cart $cart) {
+        $this->cart = $cart;
+    }
 }
--- src/Laraverse/Cart/Events/Item/Removed.php
+++ src/Laraverse/Cart/Events/Item/Removed.php
@@ -1,9 +1,17 @@
-<?php
+<?php namespace Laraverse\Cart\Events\Item;
 
-
-namespace Laraverse\Cart\Events\Item;
-
+use Laraverse\Cart\Collections\Row;
+use Laraverse\Cart\Cart;
 
 class Removed {
+    public $item;
+    public $cart;
+
+    function __construct(Row $item, Cart $cart)
+    {
+        $this->item = $item;
+        $this->cart = $cart;
+    }
+
 
 }
